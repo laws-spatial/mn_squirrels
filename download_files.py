57951f483a7d4b09c7894b9831e7666ba3269048
laws-spatial <andrewlawsmapping@gmail.com>
2023-02-26 10:39:14 -0600

    Adding files

---
+++ download_files.py
@@ -0,0 +1,32 @@
+# import libraries
+import requests
+import zipfile
+import glob
+import io
+
+# MN state administered land
+sl_url = 'https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/bdry_dnr_managed_areas/shp_bdry_dnr_managed_areas.zip'
+          
+# MN forest inventory
+forest_url = "https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/biota_dnr_forest_inv_species/shp_biota_dnr_forest_inv_species.zip"
+
+# MN land cover classification
+mnlcc_url = "https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dnr/biota_landcover_mlccs/shp_biota_landcover_mlccs.zip"
+
+print('Downloading shapefiles...')
+
+local_path = 'data/'
+url_list = [sl_url, forest_url, mnlcc_url]
+
+for index, url in enumerate(url_list):
+    print(f"Downloading shapefile {index + 1}")
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(path=local_path) # extract to folder
+
+print("Done")
+
+filenames = glob.glob("./data/*forest_inventory_species.shp")
+filenames.extend(glob.glob("./data/*management_units.shp"))
+filenames.extend(glob.glob("./data/*landcover*.shp"))
+print(f"The files of interest are {filenames}.")
